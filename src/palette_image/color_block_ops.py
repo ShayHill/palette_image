7eaf6e2c7462625350e6353f2d3c21b2ceaaf229
Shay <shay_public@hotmail.com>
2024-12-24 08:56:32 -0600

    docs: translate docstrings in color_block_ops

--- src/palette_image/color_block_ops.py
+++ src/palette_image/color_block_ops.py
@@ -1,11 +1,11 @@
-"""Operations for the color blocks on the right side of the palette.
+"""Operaciones para los bloques de color en el lado derecho de la paleta.
 
-Boxes are defined by a dist of integers. For example, [1, 1, 1, 1, 1, 2] would create
-a stack of color blocks where the first five are the same height and the last is
-twice as tall.
+Cajas definidas por una dist de enteros. Por ejemplo, [1, 1, 1, 1, 1, 2] crearía una
+pila de bloques de color donde los primeros cincos tienen la misma altura y el último
+es el doble de alto.
 
-I bend this rule for aesthetics. Firstly, by grouping the last consecutive 1s into a
-vertical stack.
+Yo doblando esta regla por estética. Primero, agrupando los últimos 1s consecutivos
+en una pila vertical.
 
 00000
 11111
@@ -13,9 +13,9 @@ vertical stack.
 33 44
 55555
 
-Secondly, by attempting to make all such vertical stacks the same height. Thirdly, by
-attempting to keep all dist=1 to the same height. Where possible, only dist values >
-1 will be stretched.
+En segundo lugar, intentanda haces todas las pilas verticales la misma altura. En
+tercer lugar, intentando mentener todas las dist=1 a la misma altura. Donde sea, solo
+dist valores > 1 serán estirados.
 
 :author: Shay Hill
 :created: 2024-11-20
@@ -27,19 +27,21 @@ import svg_ultralight as su
 
 
 def _group_double_1s(slices: list[int]) -> list[list[int]]:
-    """Working from the end of a list, group first two consecutive 1s.
+    """Trabajando desde el final de una lists, agrupa los primeros dos 1s consecutivos.
 
-    :param slices: a list of integers
-    :return: a list of lists of integers, where each list is a single integer or [1, 1]
+    :param slices: una lista de enteros
+    :return: una lista de listas de enteros, donde cada liste es un solo entero o
+        [1, 1]
 
-    Given a list of integers, when 1, 1 appears, group them together as [1, 1]. Group
-    other values as singles.
+    Dada una lista de números enteros, cuando aparezca 1, 1, agrúpelos como [1, 1].
+    Agrupar otros valores como solteros.
 
         >>> _group_double_1s([1, 2, 3, 4, 1, 1])
         [[1], [2], [3], [4], [1, 1]]
 
-    Starts from -1 and stops after the first [1, 1] is created.
-    Slices = [1, 1, 1, 1, 1, 2] -> [[1], [1], [1], [1, 1], [2]]
+    Comienza desde -1 y se detiente después  de crear el primer [1, 1].
+
+    slices = [1, 1, 1, 1, 1, 2] -> [[1], [1], [1], [1, 1], [2]]
     """
     groups: list[list[int]] = []
     for i in reversed(slices):
@@ -61,31 +63,41 @@ def _divvy_height(
 ) -> list[float]:
     """Divide a height into segments based on vertical groupings.
 
-    :param bbox: the bounding box to divide vertically
-    :param groups: a list of horizontal groupings. The output of _group_double_1s
-    :param locks: a list of tuples. Each tuple is a group and a height. For any g, h
-        in locks, the height of g will be set to h. If None, the default is to set
-        [1] to half the width and [1, 1] to the full width. List locks in order of
-        priority, starting with the highest priority. If all groups are locked, there
-        won't be any groups to scale to fill the bbox height, so divvy_height will
-        remove locks from the right of the lock sequence until scalable groups are
-        found.
+    Dividir una altura en segmentos basados en agrupaciones verticales.
 
-    :return: a list of heights summing to bbox.height
+    :param bbox: la BoundingBox para dividar verticalmente
+    :param groups: una lista de agrapaciones horizontales. La salida de
+        _group_double_1s
+    :param locks: una lista de tuplas. Cada tuple es un grupo y una altura. Para
+        cualquier g, h en locks, la altura de g se establecerá en h. Si None, el
+        valor predeterminando es establecer [1] en la mitad del ancho y [1, 1] en el
+        ancho completo. Enumere los bloqueos en orden de prioridad, comenzando por el
+        de mayor prioridad. Si todos grupos están bloqueados, no habrá ningún grupo
+        para escalar para llenar la altura del bbox, por lo que divvy_height
+        eliminará los bloqueos de la derecha de la secuencia de bloqueo hasta que se
+        encuentren grupos escalables.
 
-    _group_double_1s will group consecutive 1s in a dist into a double. This mimics
-    some of the earlier palette images in my project. Possible returned values are
-    [[3], [4], [1, 1]], [[3], [4], [2], [1]], etc. Where possible, [1, 1], and [1]
-    are set to specific heights, whereas [2], [3], [4], [5], etc. are allowed to
-    stretch to fill the vertical space.
+    :return: una lista de alturas que suman bbox.height
 
-    As of right now, _group_double_1s is intentionally restricted from producing any
-    grouping (like [[1], [1, 1]]) that would prevent locking all singles and doubles,
-    but I created this function to handle any conceivable grouping _group_double_1s
-    might produce in the future.
+    _group_double_1s agrupará 1 consecutivos en una dists en un doble. Esto imita
+    algunas de las primeras imágenes de palata en mi proyecto. Los valores posibles
+    son [[3], [4], [1, 1]], [[3], [4], [2], [1]], etc. Cuando sea posible, [1, 1] y
+    [1] se establecen en alturas específicas, mientras que [2], [3], [4], [5], etc.
+    se permiten estirar para llenar el espacio.
 
-    What _group_double_1s will never do (because nothing is implemented for this) is
-    create a group longer than 2 or a group of two with any values other than 1.
+    A partir de ahora, _group_double_1s tiene restringido intencionalmente la
+    producción de cualquier agrupación (como [[1], [1, 1]]) que impida bloquear todos
+    los individuales e dobles, preo creé esta función para manejar cualquier
+    agrupación imaginable que _group_double_1s puede producir en el futuro.
+
+    A partir de ahora, _group double_1s tiene restringido intencionalmente la
+    producción de cualquier agrupación (como [[1], [1, 1]]) que impida bloquear todos
+    los individuales y dobles, pero creé esta función para manejar cualquier
+    agrupación imaginable que _group double_1s pueda producir en el futuro.
+
+    Que _group_double_1s nunca hará (porque no se ha implementado nada para esto) es
+    crear un grupo más largo que 2 o un grupo de dos con cualquier valor que no sea
+    1.
     """
     if not groups:
         msg = "No groups to divvy in divvy_height."
@@ -122,7 +134,7 @@ def _divvy_height(
 
 
 def position_blocks(bbox: su.BoundingBox, dist: list[int]) -> Iterator[su.BoundingBox]:
-    """Yield a BoundingBox for each block in the palette."""
+    """Yield una BoundingBox para cada bloque en la paleta."""
     groups = _group_double_1s(dist)
     heights = _divvy_height(bbox, groups)
 
