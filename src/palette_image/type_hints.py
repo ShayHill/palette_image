30c8d1804885845265ba2b47acfa6726a1f011d1
Shay <shay_public@hotmail.com>
2024-12-28 21:18:08 -0600

    refactor: update type hints

---
+++ src/palette_image/type_hints.py
@@ -0,0 +1,35 @@
+"""Reimplement Python internal type hints that are not available in public modules.
+
+:author: Shay Hill
+:created: 2024-12-27
+"""
+
+from typing import Any, Protocol, TypeAlias, TypeVar
+
+
+_T_contra = TypeVar("_T_contra", contravariant=True)
+
+
+class SupportsDunderLT(Protocol[_T_contra]):
+    """Protocolo para objetos que soportan el operador de comparación "<"."""
+
+    def __lt__(self, other: _T_contra, /) -> bool:
+        """Devolver si self es menor que other."""
+        ...
+
+
+class SupportsDunderGT(Protocol[_T_contra]):
+    """Protocolo para objetos que soportan el operador de comparación ">"."""
+
+    def __gt__(self, other: _T_contra, /) -> bool:
+        """Devolver si self es mayor que other."""
+        ...
+
+
+# Toda la clasificación se realiza con __lt__ en Python, pero a *pyright* no le
+# gusta. LT y GT son necesarios para tipos integrados como int y float para que
+# coincidan con SupportsRichComparison.
+SupportsRichComparison: TypeAlias = SupportsDunderLT[Any] | SupportsDunderGT[Any]
+SupportsRichComparisonT = TypeVar(
+    "SupportsRichComparisonT", bound=SupportsRichComparison
+)
